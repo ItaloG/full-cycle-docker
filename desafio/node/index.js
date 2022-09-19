dd581fb1608ae1e6e0e829977d021102c00b6c84
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-09-19 16:32:28 -0300

    adicionando bando mysql

--- desafio/node/index.js
+++ desafio/node/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
-const app = express();
+const mysql = require("mysql");
 
+const app = express();
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) VALUES ('italo')`;
+connection.query(sql);
+
+const users = connection.query("SELECT * FROM people");
+
+connection.end();
 
 app.get("/", (req, res) => {
   res.send(`
   <h1>Full Cycle Rocks!</h1>
 
   <p>- Lista de nomes cadastrada no banco de dados.</p>
+  ${users}
   `);
 });
 
