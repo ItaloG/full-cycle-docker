5a8ed5edfd11c7485c917b0d127c1c970f13eb49
Italo Gabriel <italog03@gmail.com>
2022-09-19 21:20:21 -0300

    node challenge complete

--- desafio/node/index.js
+++ desafio/node/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const app = express();
 const config = {
@@ -13,16 +13,18 @@ const connection = mysql.createConnection(config);
 const sql = `INSERT INTO people(name) VALUES ('italo')`;
 connection.query(sql);
 
-const users = connection.query("SELECT * FROM people");
+let users;
 
-connection.end();
+connection.execute("SELECT * FROM people", (err, results, fields) => {
+  users = results;
+});
 
 app.get("/", (req, res) => {
   res.send(`
   <h1>Full Cycle Rocks!</h1>
 
   <p>- Lista de nomes cadastrada no banco de dados.</p>
-  ${users}
+  ${users.map((user) => `<p>${user.name}</p>`)}
   `);
 });
 
